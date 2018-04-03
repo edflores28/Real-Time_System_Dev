874cd28fd14aeb8124c1eb24b6c0dcb3aecb456e
Edwin Flores <edflores28@gmail.com>
2018-04-03 21:47:09 +0000

    Testing

--- Project3/main.py
+++ Project3/main.py
@@ -4,6 +4,7 @@ max_value = 2000
 min_value = 700
 
 # MAIN
+print("Initializing motors")
 motor1 = motor.Motor(4, min_value, max_value)
 motor2 = motor.Motor(14, min_value, max_value)
 motor3 = motor.Motor(24, min_value, max_value)
@@ -16,6 +17,11 @@ print("Or just hit enter to stop the motors")
 
 while True:
     userIn = input()
+    if userIn is not None:
+        userIn = int(userIn)
+    else:
+        break
+
     print("Setting motors to", userIn)
     if userIn >= min_value and userIn <= max_value:
         motor1.set_pwn(userIn)
@@ -23,4 +29,8 @@ while True:
         motor3.set_pwn(userIn)
         motor4.set_pwn(userIn)
     else:
+        motor1.stop()
+        motor2.stop()
+        motor3.stop()
+        motor4.stop()
         break
