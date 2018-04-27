f6b8b23d245cff6003c752edf17fbfcee289164b
Edwin Flores <edflores28@gmail.com>
2018-04-27 11:01:38 +0000

    Raspberry Pi Main application

---
+++ Project4/main.py
@@ -0,0 +1,45 @@
+import serial
+import queue
+import threading
+import socket
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+addr =("192.168.1.213",6879)
+
+ser = serial.Serial("/dev/ttyACM0",9600)
+ser.baudrate=9600
+
+ctl_queue = queue.Queue()
+nav_queue = queue.Queue()
+udp_queue = queue.Queue()
+
+def read_arduino(rc, imu, udp):
+    while True:
+        value = ser.readline()
+        decoded = value.decode().rstrip().split(',')
+        if decoded[0] == "CTL":
+            rc.put(decoded[1:])
+        if decoded[0] == "NAV":
+            imu.put(decoded[1:])
+        udp.put(decoded)
+
+def send_message(udp, sock):
+    while True:
+        temp = repr(udp.get())
+        b = bytearray()
+        b.extend(temp.encode())
+        print(sock.sendto(b, (addr)))
+
+worker = threading.Thread(target=read_arduino,args=(ctl_queue, nav_queue, udp_queue))
+worker.setDaemon(True)
+worker.start()
+
+worker1 = threading.Thread(target=send_message,args=(udp_queue,sock))
+worker1.setDaemon(True)
+worker1.start()
+
+while True:
+    print(ctl_queue.get())
+    print(nav_queue.get())
+
