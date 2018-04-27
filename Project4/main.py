63eaef4cea3c1d82dfcbec82c87d2198dc55d558
Edwin Flores <edflores28@gmail.com>
2018-04-27 19:41:32 -0400

    Added rc control over the escs

--- Project4/main.py
+++ Project4/main.py
@@ -2,22 +2,31 @@ import serial
 import queue
 import threading
 import socket
+import motor
+import numpy
 
+
+max_value = 2000
+min_value = 800
+
+receiver_vals = [1000, 1900]
+esc_vals = [min_value, max_value]
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-addr =("192.168.1.213",6879)
+addr =("192.168.1.213", 6879)
 
-ser = serial.Serial("/dev/ttyACM0",9600)
-ser.baudrate=9600
+ser = serial.Serial("/dev/ttyACM0", 9600)
+ser.baudrate = 9600
 
 ctl_queue = queue.Queue()
 nav_queue = queue.Queue()
 udp_queue = queue.Queue()
 
+
 def read_arduino(rc, imu, udp):
     while True:
         value = ser.readline().decode()
-        print(value) 
+        print(value)
         udp.put(''.join(value))
 
         decoded = value.rstrip().split(',')
@@ -26,22 +35,44 @@ def read_arduino(rc, imu, udp):
         if decoded[0] == "NAV":
             imu.put(decoded[1:])
 
+
 def send_message(udp, sock):
     while True:
         temp = repr(udp.get())
         b = bytearray()
         b.extend(temp.encode())
-        print(sock.sendto(b, (addr)))
+        sock.sendto(b, (addr))
 
-worker = threading.Thread(target=read_arduino,args=(ctl_queue, nav_queue, udp_queue))
+
+'''
+MAIN
+'''
+
+worker = threading.Thread(target=read_arduino, args=(ctl_queue, nav_queue, udp_queue))
 worker.setDaemon(True)
 worker.start()
 
-worker1 = threading.Thread(target=send_message,args=(udp_queue,sock))
+worker1 = threading.Thread(target=send_message, args=(udp_queue, sock))
 worker1.setDaemon(True)
 worker1.start()
 
-while True:
-    print(ctl_queue.get())
-    print(nav_queue.get())
+motor1 = motor.Motor(4, min_value, max_value)
+motor2 = motor.Motor(14, min_value, max_value)
+motor3 = motor.Motor(23, min_value, max_value)
+motor4 = motor.Motor(25, min_value, max_value)
 
+while True:
+    ctl = ctl_queue.get()
+    yaw = numpy.interp(ctl[0])
+    print(yaw, ctl[0])
+    throttle = numpy.interp(ctl[1])
+    print(throttle, ctl[1])
+    pitch = numpy.interp(ctl[2])
+    print(pitch, ctl[2])
+    roll = numpy.interp(ctl[3])
+    print(roll, ctl[3])
+    print(nav_queue.get())
+    motor1.set_pwm(yaw)
+    motor2.set_pwm(throttle)
+    motor3.set_pwm(pitch)
+    motor4.set_pwm(roll)
---
+++ Project4/motor.py
@@ -0,0 +1,30 @@
+import time
+import pigpio
+
+
+class Motor:
+    def __init__(self, pin, min_value, max_value):
+        # Variables
+        self.pin = pin
+        self.pi = pigpio.pi()
+        self.max_value = max_value
+        self.min_value = min_value
+        # Arming sequence
+        self.stop()
+        time.sleep(1)
+        self.pi.set_servo_pulsewidth(pin, max_value)
+        time.sleep(1)
+        self.pi.set_servo_pulsewidth(pin, min_value)
+        time.sleep(2)
+        # Start in an "off" state
+        self.stop()
+
+    def set_pwm(self, value):
+        self.pi.set_servo_pulsewidth(self.pin, value)
+
+    def stop(self):
+        self.pi.set_servo_pulsewidth(self.pin, 0)
+
+    def stopAll(self):
+        self.stop()
+        self.pi.stop()
